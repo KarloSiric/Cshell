9d4e776e72f4eb3870e45247b1ee6f2d72267232
ksiric <144100868+KarloSiric@users.noreply.github.com>
2026-02-23 15:06:35 +0100

    init commit

---
+++ scommon.h
@@ -0,0 +1,23 @@
+#ifndef __S_COMMON_H__
+#define __S_COMMON_H__ 
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef unsigned char byte;
+
+typedef uint8_t byte;
+typedef bool sbool;
+
+
+
+
+
+
+
+
+
+#endif
